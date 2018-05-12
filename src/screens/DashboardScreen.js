b2b882eea223c521460f22fcb62724df8d88bb1f
alishaevn <alieva.inc@gmail.com>
2018-05-11 23:07:00 -0700

    brought over more code from the web app

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,20 +1,57 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, Button } from 'react-native';
+import { connect } from 'react-redux';
 
+// import RequiresLogin from '../common/Requires-Login';
 import Navbar from '../common/Navbar';
 import UserProfile from '../common/UserProfile';
+import { getMarkersDashboard, deleteMarkerDashboard } from '../actions/markerActions';
 
 import * as dashboardStyles from '../styles/dashboardStyles';
 
 
-export default class WardrobeScreen extends React.Component {
+class DashboardScreen extends Component {
+  componentDidMount(props) {
+    this.props.dispatch(getMarkersDashboard());
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       header: 'Dashboard'
     };
   }
+
+	onClick(e) {
+		e.preventDefault();
+		console.log(e.target.id);
+		this.props.dispatch(deleteMarkerDashboard({ markerId: e.target.id }));
+  }
+  
   render() {
+    const reports = this.props.markersFromServer;
+
+		let userReports = reports.map(report => (
+			<View style={dashboardStyles.reportCard} key={report._id} >
+				{/* {this.props.loggedIn ? '' : (<Redirect to='/map' />)} */}
+				<Text style={dashboardStyles.incidentType}>{report.incidentType}</Text>
+				<Image 
+          style={dashboardStyles.reportIcon}
+          alt={`Report icon for ${report.incidentType}`} 
+          source={{uri: 'report.icon'}} />
+				<Text style={dashboardStyles.incidentDate}>Date: {report.date}</Text>
+
+				<Text style={dashboardStyles.incidentDescriptionTitle}>Description:</Text>
+				<Text style={dashboardStyles.incidentDescription}>{report.description}</Text>
+				<Button 
+          onClick={e => this.onClick(e)} 
+          id={report._id} 
+          style={dashboardStyles.deleteIncident}>
+					Delete
+				</Button>
+			</View>
+    ));
+    
     return (
       <View style={dashboardStyles.view}>
         <Navbar
@@ -23,7 +60,17 @@ export default class WardrobeScreen extends React.Component {
           back={() => this.props.navigation.goBack()}
         />
         <UserProfile/>
+        <View style={dashboardStyles.userReports}>{userReports}</View>
       </View>
     );
   }
 }
+
+export const mapStateToProps = state => ({
+	markersFromServer: state.markers.allMarkers ? state.markers.allMarkers : [],
+	loggedIn: state.auth.currentUser !== null,
+	currentUser: state.auth.currentUser ? state.auth.currentUser : '',
+});
+
+// export default RequiresLogin()(connect(mapStateToProps)(DashboardScreen));
+export default connect(mapStateToProps)(DashboardScreen);
