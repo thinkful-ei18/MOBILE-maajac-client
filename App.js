c2ebfaa69070abaec799e68f7fa0ab6094e328f2
alishaevn <alieva.inc@gmail.com>
2018-05-11 23:03:32 -0700

    changed to use navigation files

--- App.js
+++ App.js
@@ -1,38 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStackNavigator } from 'react-navigation';
-import store from './store';
-import LoginScreen from './src/screens/LoginScreen';
-import Map from './src/screens/MapScreen';
-import DashboardScreen from './src/screens/DashboardScreen';
 
-const RootStack = createStackNavigator({
-  Login: {
-    screen: LoginScreen,
-    navigationOptions: {
-      header: null
-    }
-  },
-  Map: {
-    screen: Map,
-    navigationOptions: {
-      header: null
-    }
-  },
-  Dashboard: {
-    screen: DashboardScreen,
-    navigationOptions: {
-      header: null
-    }
-  }
-});
+import { store } from './navigation/store';
+import AppWithNavigationState from './navigation/RootStack';
 
-export default class App extends React.Component {
+
+// LOADS THE ENTIRE APP
+export default class Root extends Component {
   render() {
     return (
       <Provider store={store}>
-        <RootStack />
+        <AppWithNavigationState />
       </Provider>
     );
   }
 }
+
+/*
+Resources:
+ - https://reactnavigation.org/docs/en/redux-integration.html
+*/
