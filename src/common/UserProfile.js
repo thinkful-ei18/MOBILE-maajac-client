6f0824ee31201c632793cfc19b4229cfa80ab28e
alishaevn <alieva.inc@gmail.com>
2018-05-15 13:17:37 -0700

    loads un from server && can log out

--- src/common/UserProfile.js
+++ src/common/UserProfile.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
-// import { connect } from 'react-redux';
+import { View, Text, Image, TouchableOpacity, AsyncStorage } from 'react-native';
+import { connect } from 'react-redux';
+
+// import { API_BASE_URL } from '../../config';
 
 import * as dashboardStyles from '../styles/dashboardStyles';
 
-export default class UserProfile extends Component {
+class UserProfile extends Component {
+  logout = () => {
+    AsyncStorage.clear()
+  };
+  
   render() {
 
     return (
       <View style={dashboardStyles.userProfile}>
 
-        <View style={dashboardStyles.userPicDiv}>
+        {/* <View style={dashboardStyles.userPicDiv}>
           <Image 
             source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
             alt='profile-pic' 
             style={dashboardStyles.profilePic} 
           />
-        </View>
+        </View> */}
 
         <View style={dashboardStyles.userInfoDiv}>
           <Text 
             style={dashboardStyles.userUsername} >
-            stephen30
+            {this.props.currentUser.username}
           </Text>
         </View>
+
+        <TouchableOpacity onPress={this.logout}>
+					<Text>Log Out</Text>
+				</TouchableOpacity>
 
       </View>
     );
   }
 }
 
-// export const mapStateToProps = (state, props) => ({
-//   // loggedIn: state.auth.currentUser !== null,
-//   currentUser: state.auth.currentUser ? state.auth.currentUser : '',
-//   // ppModal: state.modal.ppModal,
-//   profilePicture: state.auth.currentUser.profilePicture ? state.auth.currentUser.profilePicture : 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
-// });
+export const mapStateToProps = state => ({
+  // loggedIn: state.auth.currentUser !== null,
+  currentUser: state.auth.currentUser ? state.auth.currentUser : '',
+  // ppModal: state.modal.ppModal,
+  // profilePicture: state.auth.currentUser.profilePicture ? state.auth.currentUser.profilePicture : 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
+});
 
-// export default connect(mapStateToProps)(UserProfile);
+export default connect(mapStateToProps)(UserProfile);
 
 
 /*
