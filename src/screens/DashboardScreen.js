5a8a96ac2f856b97a440492aeb7b774c3521b7fb
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-05-15 14:24:44 -0700

    redirection working

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text, Image, Button, ScrollView, /* AsyncStorage */ } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  ScrollView /* AsyncStorage */
+} from 'react-native';
 import { connect } from 'react-redux';
 
 // import RequiresLogin from '../common/Requires-Login';
 import Navbar from '../common/Navbar';
 import UserProfile from '../common/UserProfile';
-import { getMarkersDashboard, deleteMarkerDashboard } from '../actions/markerActions';
+import {
+  getMarkersDashboard,
+  deleteMarkerDashboard
+} from '../actions/markerActions';
 import { getCurrentUser } from '../actions/userActions';
 
 import * as styles from '../styles/dashboardStyles';
@@ -23,37 +32,36 @@ class DashboardScreen extends Component {
     };
   }
 
-	onClick(markerId) {
-		this.props.dispatch(deleteMarkerDashboard({ markerId }));
+  onClick(markerId) {
+    this.props.dispatch(deleteMarkerDashboard({ markerId }));
   }
 
   render() {
     const reports = this.props.markersFromServer;
     let username;
 
-
-		let userReports = reports.map(report => (
-			<View style={styles.reportCard} key={report._id} >
-				{/* {this.props.loggedIn ? '' : (<Redirect to='/map' />)} */}
-          <Text style={styles.incidentType}>{report.incidentType}</Text>
-          <Image 
-            style={styles.reportIcon}
-            alt={`Report icon for ${report.incidentType}`} 
-            source={{uri: 'report.icon'}} 
-          />
-          <Text style={styles.incidentDate}>Date: {report.date}</Text>
-          <Text style={styles.incidentDescriptionTitle}>Description:</Text>
-          <Text style={styles.incidentDescription}>{report.description}</Text>
-          <Button 
-            onPress={() => this.onClick(report._id)}
-            title='Delete'
-            id={report._id} 
-            style={styles.deleteIncident}
-            accessibilityLabel='Delete this incident report'
-            >
-            Delete
-          </Button>
-			</View>
+    let userReports = reports.map(report => (
+      <View style={styles.reportCard} key={report._id}>
+        {/* {this.props.loggedIn ? '' : (<Redirect to='/map' />)} */}
+        <Text style={styles.incidentType}>{report.incidentType}</Text>
+        <Image
+          style={styles.reportIcon}
+          alt={`Report icon for ${report.incidentType}`}
+          source={{ uri: 'report.icon' }}
+        />
+        <Text style={styles.incidentDate}>Date: {report.date}</Text>
+        <Text style={styles.incidentDescriptionTitle}>Description:</Text>
+        <Text style={styles.incidentDescription}>{report.description}</Text>
+        <Button
+          onPress={() => this.onClick(report._id)}
+          title="Delete"
+          id={report._id}
+          style={styles.deleteIncident}
+          accessibilityLabel="Delete this incident report"
+        >
+          Delete
+        </Button>
+      </View>
     ));
 
     return (
@@ -63,7 +71,7 @@ class DashboardScreen extends Component {
           plus={() => this.add()}
           back={() => this.props.navigation.goBack()}
         />
-        <UserProfile/>
+        <UserProfile navigation={this.props.navigation} />
         <ScrollView>
           <View style={styles.userReports}>{userReports}</View>
         </ScrollView>
@@ -74,11 +82,11 @@ class DashboardScreen extends Component {
 
 export const mapStateToProps = state => {
   console.log('DASHBOARD SCREEN STATE:', state);
-	return {
+  return {
     markersFromServer: state.markers.allMarkers ? state.markers.allMarkers : [],
     loggedIn: state.auth.currentUser !== null,
-    currentUser: state.auth.currentUser ? state.auth.currentUser : '',
-  }
+    currentUser: state.auth.currentUser ? state.auth.currentUser : ''
+  };
 };
 
 export default connect(mapStateToProps)(DashboardScreen);
@@ -87,4 +95,3 @@ export default connect(mapStateToProps)(DashboardScreen);
 Resources:
  - https://facebook.github.io/react-native/docs/button.html
 */
-
