5993c7a6f4030a7b5751afbf556059b01f97a7c3
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-05-16 14:01:40 -0700

    date and picker

--- src/common/Report.js
+++ src/common/Report.js
@@ -1,155 +1,185 @@
 import React, { Component } from 'react';
-import {
-  Text,
-  ScrollView,
-  TextInput,
-  View,
-  AsyncStorage,
-  TouchableOpacity
-} from 'react-native';
+import { Text, ScrollView, TextInput, View, AsyncStorage, TouchableOpacity, Picker } from 'react-native';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { newMarker, getMarkers } from '../actions/markerActions';
+import DatePicker from 'react-native-datepicker';
 
 import Input from './Input';
 
-
-import * as style from '../styles/reportFormStyles'
+import * as style from '../styles/reportFormStyles';
 
 class reportForm extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      incidentType: '',
-      date: '',
-      time: '',
-      description: ''
-    };
-  }
+		this.state = {
+			incidentType: '',
+			date: '',
+			time: '',
+			description: '',
+			isVisible: false,
+		};
+	}
 
-  handleLocationError(error) {
-    this.setState({ locationError: error });
-  }
+	_showDateTimePicker = () => this.setState({ isVisible: true });
 
-  userAuth = async () => {
-    let authToken = await AsyncStorage.getItem('authToken');
-  };
+	_hideDateTimePicker = () => this.setState({ isVisible: false });
 
-  submit() {
-    return AsyncStorage.getItem('authToken')
-      .then(authToken =>
-        fetch('https://safer-server.herokuapp.com/api/new/marker', {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json',
-            Authorization: `Bearer ${authToken}`
-          },
-          body: JSON.stringify({
-            incidentType: this.state.incidentType,
-            location: this.props.location,
-            date: this.state.date,
-            description: this.state.description,
-            time: this.state.time
-          })
-        })
-      )
-      .then(res => res.json());
-  }
+	_handleDatePicked = date => {
+		console.log('A date has been picked: ', date);
+		this._hideDateTimePicker();
+	};
 
-  render() {
-    return (
-      <ScrollView
-        style={style.reportFormContainer}>
-        <Text
-          style={style.label}>Incident Type</Text>
-        <TextInput
-          style={style.input}
-          value={this.state.incidentType}
-          onChangeText={value =>
-            this.setState({
-              incidentType: value
-            })
-          }
-        />
-        <Text
-          style={style.label}>Date</Text>
-        <TextInput
-          style={style.input}
-          value={this.state.date}
-          onChangeText={value =>
-            this.setState({
-              date: value
-            })
-          }
-        />
-        <Text
-          style={style.label}>Time</Text>
-        <TextInput
-          style={style.input}
-          value={this.state.time}
-          onChangeText={value => this.setState({ time: value })}
-        />
-        <Text
-          style={style.label}>Description of Incident</Text>
-        <TextInput
-          style={style.input}
-          value={this.state.description}
-          multiline={true}
-          onChangeText={value =>
-            this.setState({
-              description: value
-            })
-          }
-        />
-        <TouchableOpacity
-          onPress={() => {
-            this.props.close();
-            this.submit()
-            .then(() => {
-            console.log('Getting new markers...');
-            this.props.dispatch(getMarkers())});
-          }}
-          style={style.button}
-          text={'Submit'}
-          accessibilityLabel={'Submit'}>
-          <Text>Submit</Text>
-        </TouchableOpacity>
+	handleLocationError(error) {
+		this.setState({ locationError: error });
+	}
 
-        <View style={style.buttonContainer}>
-          <TouchableOpacity
-            onPress={() =>
-              this.setState({
-                incidentType: '',
-                date: '',
-                time: '',
-                description: ''
-              })
-            }
-            style={style.buttonWarning}>
-            <Text>Clear</Text>
-          </TouchableOpacity>
+	userAuth = async () => {
+		let authToken = await AsyncStorage.getItem('authToken');
+	};
 
+	submit() {
+		return AsyncStorage.getItem('authToken')
+			.then(authToken =>
+				fetch('https://safer-server.herokuapp.com/api/new/marker', {
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/json',
+						Authorization: `Bearer ${authToken}`,
+					},
+					body: JSON.stringify({
+						incidentType: this.state.incidentType,
+						location: this.props.location,
+						date: this.state.date,
+						description: this.state.description,
+						time: this.state.time,
+					}),
+				})
+			)
+			.then(res => res.json());
+	}
 
-          <TouchableOpacity
-            onPress={this.props.close}
-            style={style.buttonCancel}
-            accessibilityLabel={'Cancel'}
-            text={'Cancel'}>
-            <Text>Cancel</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    );
-  }
+	render() {
+		return (
+			<ScrollView style={style.reportFormContainer}>
+				<Text style={style.label}>Incident Type</Text>
+				<Picker
+					selectedValue={this.state.incidentType}
+					onValueChange={itemValue => this.setState({ incidentType: itemValue })}
+				>
+					<Picker.Item label="Theft" value="Theft" />
+					<Picker.Item label="Crime" value="Crime" />
+					<Picker.Item label="Accident" value="Accident" />
+					<Picker.Item label="Road Construction" value="Road-Construction" />
+					<Picker.Item label="Other" value="Other" />
+				</Picker>
+				<Text style={style.label}>Date</Text>
+				<DatePicker
+					style={{ width: 200 }}
+					date={this.state.date}
+					mode="date"
+					placeholder="select date"
+					format="MM-DD-YYYY"
+					minDate="2016-05-01"
+					maxDate="2016-06-01"
+					confirmBtnText="Confirm"
+					cancelBtnText="Cancel"
+					customStyles={{
+						dateIcon: {
+							position: 'absolute',
+							left: 0,
+							top: 4,
+							marginLeft: 0,
+						},
+						dateInput: {
+							marginLeft: 36,
+						},
+						// ... You can check the source to find the other keys.
+					}}
+					onDateChange={date => {
+						this.setState({ date: date });
+					}}
+				/>
+				<Text style={style.label}>Time</Text>
+				<DatePicker
+					style={{ width: 200 }}
+					date={this.state.time}
+					mode="time"
+					placeholder="select time"
+					confirmBtnText="Confirm"
+					cancelBtnText="Cancel"
+					customStyles={{
+						dateIcon: {
+							position: 'absolute',
+							left: 0,
+							top: 4,
+							marginLeft: 0,
+						},
+						dateInput: {
+							marginLeft: 36,
+						},
+						// ... You can check the source to find the other keys.
+					}}
+					onDateChange={date => {
+						this.setState({ time: date });
+					}}
+				/>
+				<Text style={style.label}>Description of Incident</Text>
+				<TextInput
+					style={style.input}
+					value={this.state.description}
+					multiline={true}
+					onChangeText={value =>
+						this.setState({
+							description: value,
+						})}
+				/>
+				<TouchableOpacity
+					onPress={() => {
+						this.props.close();
+						this.submit().then(() => {
+							console.log('Getting new markers...');
+							this.props.dispatch(getMarkers());
+						});
+					}}
+					style={style.button}
+					text={'Submit'}
+					accessibilityLabel={'Submit'}
+				>
+					<Text>Submit</Text>
+				</TouchableOpacity>
+
+				<View style={style.buttonContainer}>
+					<TouchableOpacity
+						onPress={() =>
+							this.setState({
+								incidentType: '',
+								date: '',
+								time: '',
+								description: '',
+							})}
+						style={style.buttonWarning}
+					>
+						<Text>Clear</Text>
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						onPress={this.props.close}
+						style={style.buttonCancel}
+						accessibilityLabel={'Cancel'}
+						text={'Cancel'}
+					>
+						<Text>Cancel</Text>
+					</TouchableOpacity>
+				</View>
+			</ScrollView>
+		);
+	}
 }
 
 const mapStateToProps = state => ({
-  location: state.report.userLocation
+	location: state.report.userLocation,
 });
 
-export default reduxForm({ form: 'login' })(
-  connect(mapStateToProps)(reportForm)
-);
-
-
+export default reduxForm({ form: 'login' })(connect(mapStateToProps)(reportForm));
