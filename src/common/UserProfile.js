93f116176575b59383e76e9c21fb92dee74391c1
alishaevn <alieva.inc@gmail.com>
2018-05-16 11:44:16 -0700

    done

--- src/common/UserProfile.js
+++ src/common/UserProfile.js
@@ -2,38 +2,25 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  Image,
   TouchableOpacity,
   AsyncStorage
 } from 'react-native';
 import { connect } from 'react-redux';
 
-// import { API_BASE_URL } from '../../config';
 import { clearAuth } from '../actions/userActions';
 
 import * as dashboardStyles from '../styles/dashboardStyles';
 
 class UserProfile extends Component {
   logout = () => {
-    console.log('logging OUT!')
     AsyncStorage.clear();
     this.props.dispatch(clearAuth());
   };
 
   render() {
-    // if (this.props.currentUser === '') {
-    //   this.props.navigation.navigate('Login');
-    // }
 
     return (
       <View style={dashboardStyles.userProfile}>
-        {/* <View style={dashboardStyles.userPicDiv}>
-          <Image 
-            source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}
-            alt='profile-pic' 
-            style={dashboardStyles.profilePic} 
-          />
-        </View> */}
 
         <View style={dashboardStyles.userInfoDiv}>
           <Text style={dashboardStyles.userUsername}>
@@ -55,15 +42,7 @@ class UserProfile extends Component {
 }
 
 export const mapStateToProps = state => ({
-  // loggedIn: state.auth.currentUser !== null,
   currentUser: state.auth.currentUser ? state.auth.currentUser : ''
-  // ppModal: state.modal.ppModal,
-  // profilePicture: state.auth.currentUser.profilePicture ? state.auth.currentUser.profilePicture : 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'
 });
 
 export default connect(mapStateToProps)(UserProfile);
-
-/*
-Resources:
- - https://facebook.github.io/react-native/docs/image.html
-*/
